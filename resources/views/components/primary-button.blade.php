373046d5fa74fea74f585d4249343ab16d3ee200
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2025-02-03 19:34:28 +0330

    Add some components
    + button-group, file-upload, gradient-button
    
    + Update primary-button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,10 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-purple-800 dark:bg-purple-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-purple-800 uppercase tracking-widest hover:bg-purple-700 dark:hover:bg-white focus:bg-purple-700 dark:focus:bg-white active:bg-purple-900 dark:active:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 dark:focus:ring-offset-purple-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge([
+    'type' => 'submit',
+    'class' => 'inline-flex items-center px-4 py-2 bg-purple-800
+    dark:bg-purple-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-purple-800
+    uppercase tracking-widest hover:bg-purple-700 dark:hover:bg-white focus:bg-purple-700 dark:focus:bg-white
+    active:bg-purple-900 dark:active:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-500
+    focus:ring-offset-2 dark:focus:ring-offset-purple-800 transition ease-in-out duration-150'
+    ]) }}>
     {{ $slot }}
 </button>
