838805df7433caf9772bf883937f2ebe493614e9
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2025-06-28 17:39:19 +0330

    WIP

--- app/Filament/Resources/CourseResource/Widgets/CourseOverview.php
+++ app/Filament/Resources/CourseResource/Widgets/CourseOverview.php
@@ -10,8 +10,16 @@ class CourseOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        $total_count_trend = Trend::model(Course::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
         return [
-            Stat::make(__('courses.widgets.total_count'), forhumans(Course::withTrashed()->count()))->icon('heroicon-o-academic-cap')->color('primary'),
+            Stat::make(__('courses.widgets.total_count'), forhumans(Course::withTrashed()->count()))->icon('heroicon-o-academic-cap')->color('primary')->chart([50, 60, 30, 20, 70]),
             Stat::make(__('courses.widgets.total_enrolls'), forhumans(Course::withCount('enrolls')->get(['enrolls_count'])->sum('enrolls_count')))->icon('heroicon-o-users')->color('info'),
         ];
     }
